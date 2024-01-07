a974e182f6001d6bb3994cb0fc0572eee944107f
yunyoung1819 <yunyoung1819@hanmail.net>
2024-01-08 01:10:50 +0900

    :sparkles: feat: 쿠폰 발급 로직의 문제점

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,16 +31,30 @@ class ApplyServiceTest {
 	}
 
 	@Test
-	public void 여러명응모() {
+	public void 여러명응모() throws InterruptedException {
+
 		int threadCount = 1000;
+
 		ExecutorService executorService = Executors.newFixedThreadPool(32);
+
 		CountDownLatch latch = new CountDownLatch(threadCount);
 
 		for (int i = 0; i < threadCount; i++) {
 			long userId = i;
 			executorService.submit(() -> {
-				applyService.apply(userId);
+				try {
+					applyService.apply(userId);
+				} finally {
+					latch.countDown();
+				}
 			});
 		}
+		latch.await();
+
+		// 기대 결과는 100개의 쿠폰 발급이 되는 것이지만 Race condition이 발생힉기 때믄에 테스트 코드는 실패
+		// Race Condition은 두개 이상의 쓰레드가 공유 데이터를 엑세스하고 동시에 작업하려고 할 때 발생하는 문제
+		long count = couponRepository.count();
+
+		assertThat(count).isEqualTo(100);
 	}
 }
